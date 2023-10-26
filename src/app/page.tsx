d26e966d7c7de81a36bf548f9c99aba053ff3c77
ShurinokOleksandr <shurunok0@gmail.com>
2023-10-26 23:34:43 +0300

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 import { ThemeProvider } from 'styled-components';
-import { useRouter } from 'next/navigation';
 import { darkTheme } from '@/theme/colors';
 
 
 export default function Home() {
-	const router  = useRouter();
 	
 	return (
 		<ThemeProvider theme={darkTheme}>
