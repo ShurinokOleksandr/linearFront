75fa4de02faf4c25063b72eb6ffb8ed03ee35a8f
shuru <shurunok0@gmail.com>
2024-02-07 02:31:15 +0200

    Login

--- src/app/login/components/LoginForm/LoginForm.tsx
+++ src/app/login/components/LoginForm/LoginForm.tsx
@@ -31,6 +31,7 @@ type ValidationLoginForm = z.infer<typeof schema>;
 
 export const LoginForm = observer(() => {
     // const [isLoading, errors, handleSubmit, register, loginFn] = useLogin();
+
     const {
         formState: { errors },
         handleSubmit,
